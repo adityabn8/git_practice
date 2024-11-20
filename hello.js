baa86dac143db13f1d8cb9b052b2e0998fea746d
Aditya N <86728321+adityabn8@users.noreply.github.com>
2024-11-20 07:45:14 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 console.log("Hello, world!!");
 
-console.log("Updated for this file for second commit");
+console.log("Updated for this file for second commit remotely changed.");
 
 console.log("Remote changes");
